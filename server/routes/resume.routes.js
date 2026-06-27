102d4d725fec205eed6489eced493f4e4efbefe3
Saithrishadaggupati <saithrishadaggupati@gmail.com>
2026-06-27 22:59:29 +0530

    fix: overwrite resume routes cleanly

--- server/routes/resume.routes.js
+++ server/routes/resume.routes.js
@@ -1,4 +1,4 @@
-const express = require("express");
+﻿const express = require("express");
 const router = express.Router();
 const fs = require("fs");
 const upload = require("../middleware/upload.middleware");
