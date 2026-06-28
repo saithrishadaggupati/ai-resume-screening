8558d19302aea0f1c753a6a49ec335d4dcebf8ed
Saithrishadaggupati <saithrishadaggupati@gmail.com>
2026-06-28 10:48:41 +0530

    fix: education mixed type and auth pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,25 +1,37 @@
-import { Routes, Route } from "react-router-dom";
-
+import { Routes, Route, Navigate } from "react-router-dom";
 import Navbar from "./components/Navbar";
-
 import Dashboard from "./pages/Dashboard";
 import Jobs from "./pages/Jobs";
 import UploadResume from "./pages/UploadResume";
 import SearchResume from "./pages/SearchResume";
 import Resumes from "./pages/Resumes";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+
+function PrivateRoute({ children }) {
+  const token = localStorage.getItem("token");
+  return token ? children : <Navigate to="/login" />;
+}
 
 export default function App() {
   return (
-    <>
-      <Navbar />
-
-      <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/jobs" element={<Jobs />} />
-        <Route path="/upload" element={<UploadResume />} />
-        <Route path="/search" element={<SearchResume />} />
-        <Route path="/resumes" element={<Resumes />} />
-      </Routes>
-    </>
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/*" element={
+        <PrivateRoute>
+          <>
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/jobs" element={<Jobs />} />
+              <Route path="/upload" element={<UploadResume />} />
+              <Route path="/search" element={<SearchResume />} />
+              <Route path="/resumes" element={<Resumes />} />
+            </Routes>
+          </>
+        </PrivateRoute>
+      } />
+    </Routes>
   );
 }
