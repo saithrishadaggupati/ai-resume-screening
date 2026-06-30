35dbb7f2b41e8e97121d381eff78a7146237ae8c
Saithrishadaggupati <saithrishadaggupati@gmail.com>
2026-06-30 11:34:23 +0530

    Update API URL to Cloud Run

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://ai-resume-screening-721141274431.us-central1.run.app/api",
 });
 
 export default api;
